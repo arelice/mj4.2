00db70811e91fc2fd965c78f28fdaa9cd21a76c6
John Doe <2411728460@qq.com>
2024-07-08 01:27:12 +0800

    first commit

---
+++ src/components/custom/GithubSite.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-neutral-400">
+    <span>RJLAPI</span>
+    <a href="https://api.raojialong.space" target="_blank" class="text-blue-500">
+      GitHub
+    </a>
+  </div>
+</template>
---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+import { t } from '@/locales'
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://logo.raojialong.space/logo88.png',
+      name:  t('RJLAPI'),//'AI绘图',
+      description: '解锁全部功能 <a href="https://api.raojialong.space" class="text-blue-500" target="_blank" >购买地址</a>',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
