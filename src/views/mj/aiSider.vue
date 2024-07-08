999883b470a53574477c167a7e4902501e5f4db9
arelice <141432992+arelice@users.noreply.github.com>
2024-07-08 08:21:28 +0800

    Update aiSider.vue

--- src/views/mj/aiSider.vue
+++ src/views/mj/aiSider.vue
@@ -78,7 +78,7 @@ const chatId= computed(()=>chatStore.active??'1002' );
                     {{$t('mjtab.drawinfo')}}
                 </n-tooltip>
             </a>
-           <a :href="`https://api.raojialong.space/login`" class="flex items-center justify-center flex-col">
+           <a :href="`https://api.raojialong.space/topup`" class="flex items-center justify-center flex-col">
       <SvgIcon icon="whh:spiderman" class="text-3xl"></SvgIcon>
       <div class="text-[13px]">控制台</div>
     </a>
