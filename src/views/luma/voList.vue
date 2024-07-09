54a0cf958a54708b6cfae22eeda74479fb338410
arelice <141432992+arelice@users.noreply.github.com>
2024-07-10 06:40:35 +0800

    Update voList.vue

--- src/views/luma/voList.vue
+++ src/views/luma/voList.vue
@@ -1,104 +1,179 @@
 <script setup lang="ts">
-import { LumaMedia, lumaStore } from '@/api/lumaStore';
-import { computed, ref, watch } from 'vue';
-import {NEmpty ,NButton,NPopover, NTag,NButtonGroup } from 'naive-ui'
-import {FeedLumaTask, lumaFetch, mlog} from '@/api';
-import { homeStore } from '@/store';
+import { ref, computed, onMounted, watch } from 'vue';
+import { NInput,NButton, useMessage,NTag ,NPopover,NSelect} from 'naive-ui';
 import {SvgIcon} from '@/components/common'
-import { myTestTranscode } from '@/api/mp4img';
-//import { myTestTranscode } from '@/api/mp4img';
+import { FeedLumaTask, lumaFetch, mlog, upImg } from '@/api';
+import { gptServerStore, homeStore } from '@/store';
+import { t } from '@/locales';
+import { LumaMedia, lumaHkStore } from '@/api/lumaStore';
+import { sleep } from '@/api/suno';
 
-const st= ref({pIndex:-1});
-const list= ref<LumaMedia[]>([]);
-const csuno= new lumaStore()
-const initLoad=()=>{
-    let arr = csuno.getObjs();
-    list.value= arr.reverse()
-}
-const nowTime= computed(()=>{
-    return new Date().getTime()
-})
-const FeedLumaTaskDown=async (item:LumaMedia)=>{
-    //FeedLumaTask(id)
-    let id= item.id;
-    let url='';
-    try{
-        let d:any= await lumaFetch('/generations/'+id+'/download_video_url' );
-        mlog("d", d )
-        url=d.url?? item.video?.url
-    }catch(e){
-         url= item.video?.url??''
-    }
-     
-        //window.open(d.url)
-    const link = document.createElement('a');
-    link.href = url ;
-    link.download = id+".mp4";
-    link.target = '_blank';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    
-}
+const luma= ref({ "aspect_ratio": "16:9", "expand_prompt": true,  "image_url": "",  "user_prompt": "" });
+const st= ref({isDo:false,version:'relax'})
+const ms = useMessage();
+const fsRef= ref() ;
+const exLuma= ref<LumaMedia>()
 
-//getLastFrameBase64
-const extend= async(item:LumaMedia )=>{
-    
-    mlog("extend ", item ) 
-    homeStore.setMyData({act:"luma.extend", actData: item  })
+const vf=[{s:'width: 100%; height: 100%;',label:'1:1'}
+,{s:'width: 100%; height: 75%;',label:'4:3'}
+,{s:'width: 75%; height: 100%;',label:'3:4'}
+,{s:'width: 100%; height: 50%;',label:'16:9'}
+,{s:'width: 50%; height: 100%;',label:'9:16'}
+ ];
 
-}
-
-
-watch(()=>homeStore.myData.act, (n)=>{
-     if(n=='FeedLumaTask')  initLoad() 
+onMounted(() => {
+    homeStore.setMyData({ms:ms})
+    st.value.version= gptServerStore.myData.IS_LUMA_PRO?'pro':'relax'
 });
 
-initLoad();
-</script>
-<template>
-<div v-if="list.length>0" class="p-4">
-    <div  class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
-        <div v-for="(item, index) in list" :key="index" class="relative" @mousemove="st.pIndex=index" @mouseout="st.pIndex=-1">
-            <div class="relative flex items-center justify-center bg-white bg-opacity-10 rounded-[16px] overflow-hidden aspect-[16/8.85] ">
-                <video v-if="item.video?.url|| item.video?.download_url" :src="item.video?.download_url? item.video?.download_url:item.video?.url" @error="$event.target.src=item.video?.url" loop  playsinline  :controls="st.pIndex==index" class="w-full h-full object-cover"></video>
-                <div class=" text-center" v-else>
-                    <div v-if="item.state=='failed'" class="pt-2" >{{ $t('video.failed') }}</div> 
-                    <NButton  size="small" type="primary" @click="FeedLumaTask( item.id )"   v-else-if="!item.last_feed|| ((new Date().getTime())-item.last_feed)>20*1000" >{{$t('video.repeat')}}</NButton>
-                    <div class="pt-2" v-else>
-                        <div>{{$t('video.process')}}{{ new Date(item.last_feed).toLocaleString() }}</div>
-                        <div v-if="item.state=='pending'">{{ $t('video.pending') }}</div> 
-                        <div v-if="item.state=='processing'">{{ $t('video.processing') }}</div> 
 
-                    </div>
-                   
+
+const canPost = computed(() => {
+    return luma.value.user_prompt!='' && !st.value.isDo
+})
+const generate= async ()=>{
+    mlog("generate", luma.value )
+    st.value.isDo= true
+    if(!canPost){
+        ms.error( t('video.plsInput') )
+        return ;
+    }
+    try{
+        let url= '/generations/';
+        if(exLuma.value) url= `/generations/${exLuma.value.id}/extend`
+        //homeStore.myData.is_luma_pro?'/pro':''
+        //if(homeStore)
+        const is_luma_pro=homeStore.myData.is_luma_pro
+        if (is_luma_pro) url= '/pro'+url
+        const d:any=  await lumaFetch(url, luma.value);
+        mlog("d", d )
+        // if(d.id ) FeedLumaTask(d.id )
+        // else FeedLumaTask(d[0].id )
+        
+        const taskID= d.id??d[0].id
+        if( is_luma_pro ){
+            const hk= new lumaHkStore();
+            hk.save({id:taskID,isHK:true})
+        }
+       
+        
+        ms.success( t('video.submitSuccess'))
+         await sleep(500)
+        FeedLumaTask(taskID)
+    }catch(e){
+        
+    }
+    st.value.isDo= false
+    //FeedLumaTask('33ace512-9a46-40ab-9d08-a05eff989831')
+}
+
+function selectFile(input:any){
+     
+    upImg(input.target.files[0]).then(d=>{
+        luma.value.image_url= d;
+        fsRef.value=''
+    }).catch(e=>ms.error(e));
+    
+}
+
+const clearInput = ()=>{
+    luma.value.user_prompt= ''
+    luma.value.image_url= ''
+    exLuma.value= undefined
+}
+
+//luma.extend
+watch(()=>homeStore.myData.act, (n)=>{
+    if(n=='luma.extend'){
+        mlog("luma.extend", homeStore.myData.actData )
+        const s= homeStore.myData.actData as LumaMedia
+        exLuma.value= s 
+        // cs.value.continue_clip_id= s.id
+        // cs.value.continue_at= Math.ceil(s.metadata.duration/2) 
+    }
+});
+
+const isHK= computed(()=> {
+    const url= gptServerStore.myData.LUMA_SERVER.toLocaleLowerCase();
+    if(url!=''){
+     return (url.indexOf('hk')>-1 &&  url.indexOf('pro')==-1 ) ;
+    }
+   
+    return (homeStore.myData.session && homeStore.myData.session.isHk) ;
+    
+} );
+const saveMyDate=(is_pro:boolean)=>{
+    homeStore.setMyData({is_luma_pro: is_pro})
+    gptServerStore.setMyData({IS_LUMA_PRO: is_pro})
+}
+
+watch(()=>isHK.value , (n)=>    saveMyDate( n && st.value.version=='pro' ) ); 
+watch(()=>st.value.version , ()=>  saveMyDate(isHK.value && st.value.version=='pro' ) );
+
+const mvOption= [
+{label: '版本: relax, 价格实惠',value: 'relax'}
+,{label:'版本: pro, 快且无水印',value: 'pro'}
+ ]
+
+</script>
+
+<template>
+<div class="p-2"> 
+    <div class=" flex items-center justify-between space-x-1">
+            <template  v-for="(item,index) in vf" >
+            <section class="aspect-item flex-1 rounded border-2 dark:border-neutral-700 cursor-pointer"  :class="{'active':luma.aspect_ratio==item.label}"  @click="luma.aspect_ratio=item.label">
+                <div class="aspect-box-wrapper mx-auto my-2 flex h-5 w-5 items-center justify-center">
+                    <div class="aspect-box rounded border-2 dark:border-neutral-700" :style="item.s"></div>
                 </div>
-            </div>
-            <div class="flex justify-between items-center">
-                <div  >
+                <p class="mb-1 text-center text-sm">{{ item.label }}</p>
+            </section>
+            </template>
+
+    </div>
+    <div class="pt-1">
+      <n-input v-model:value="luma.user_prompt" 
+                :placeholder="$t('video.descpls')"  type="textarea"  size="small"   
+                :autosize="{ minRows: 3, maxRows: 12  }"  />
+    </div>
+    <div v-if="exLuma" class="pt-1">
+        <div class="flex justify-between items-center">
+            <div  >
                 <n-popover trigger="hover">
                     <template #trigger>
-                    <div class="line-clamp-1">{{item.prompt}}</div>
+                    <div class="line-clamp-1">{{ $t('video.extend') }}:{{exLuma.prompt}}</div>
                     </template>
-                    <div class=" max-w-[300px]">{{item.prompt}}</div>
+                    <div class=" max-w-[300px]">{{exLuma.prompt}}</div>
                 </n-popover>
-                
-                </div>
-                <div class="flex justify-end items-center pt-1"  v-if="item.video?.url|| item.video?.download_url"> 
-                     <!-- <span    @click="FeedLumaTaskDown( item.id )" class="cursor-pointer" ><SvgIcon icon="mdi:download" /></span> -->
+            </div>
+        </div>
+        <div class="relative flex items-center justify-center bg-white bg-opacity-10 rounded-[5px] overflow-hidden aspect-[16/8.85] ">
+                <video v-if="exLuma.video?.url|| exLuma.video?.download_url" :src="exLuma.video?.download_url? exLuma.video?.download_url:exLuma.video?.url" @error="$event.target.src=exLuma.video?.url" loop  playsinline  controls class="w-full h-full object-cover"></video>    
+        </div>
+            
+    </div>
 
-                    
-                      <n-button-group size="tiny">
-                        <n-button  size="tiny" round ghost   @click="FeedLumaTaskDown( item )"  ><SvgIcon icon="mdi:download" /> {{ $t('video.download') }}</n-button>
-                        <n-button   size="tiny"  round ghost  @click="extend( item )"  ><SvgIcon icon="ri:video-add-line" /> {{ $t('video.extend') }}</n-button>
-                      </n-button-group>
-                     <!-- <a :href="item.video?.download_url? item.video?.download_url:item.video?.url" download  target="_blank" v-if="item.video?.url|| item.video?.download_url"  ><SvgIcon icon="mdi:download" class="cursor-pointer"/></a> -->
+     <div  class="pt-1" v-if="isHK">
+        <n-select v-model:value="st.version" :options="mvOption" size="small" />
+    </div>
+    
+    <div class="pt-1">
+        <div class="flex justify-between  items-end">
+            <div> 
+                <input type="file"  @change="selectFile"  ref="fsRef" style="display: none" accept="image/jpeg, image/jpg, image/png, image/gif"/>
+                <div class="h-[80px] w-[80px] overflow-hidden rounded-sm border border-gray-400/20 flex justify-center items-center cursor-pointer" @click=" fsRef.click()">
+                    <img :src="luma.image_url" v-if="luma.image_url" />
+                    <div class="text-center" v-else>{{ $t('video.selectimg') }}</div> 
+                </div>
+            </div>
+            <div>
+                <div class="pb-1 text-right">
+                  <NTag v-if=" exLuma|| luma.user_prompt!=''||luma.image_url!=''" type="success" size="small" round  ><span class="cursor-pointer" @click="clearInput()" >{{$t('video.clear')}}</span></NTag>
+                </div>
+                <div>
+                    <NButton  :loading="st.isDo" type="primary" :disabled="!canPost" @click="generate()"><SvgIcon icon="ri:video-add-line"  /> {{$t('video.generate')}}</NButton> 
                 </div>
             </div>
         </div>
-    </div>
-</div>
-<div class="w-full h-full flex justify-center items-center" v-else>
-    <NEmpty :description="$t('video.nodata')"></NEmpty>
+    </div>    
 </div>
 </template>
